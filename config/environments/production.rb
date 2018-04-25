0de3054e7b34370f3d4f971879558678e4c29960
佐藤淳司 <junji@iMac.local>
2018-04-25 19:06:38 +0900

    Add production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,16 +79,15 @@ Rails.application.configure do
 
   #remenber to change localhost to actual host
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   config.paperclip_defaults = {
-    storage: :s3,
-    s3_protocol: :https,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+    :storage        => :s3,
+    :bucket         => ENV['S3_BUCKET_NAME'],
+    :s3_region      => ENV['AWS_REGION'],
+    :s3_host_name   => 's3-ap-northeast-1.amazonaws.com',
+    :s3_credentials => {
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
-
 end
